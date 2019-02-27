53f1669b9cdd4675edad8dd9c7b2c7da8ea7ffe1
Rychard <rychard34@outlook.com>
2019-02-27 16:42:44 -0300

    lendo e escrevendo no arquivo csv

--- Aulas/lendocsv.py
+++ Aulas/lendocsv.py
@@ -7,6 +7,6 @@ for linha in reader:
     media = (float(linha[2]) + float(linha[3])) / 2
     medias.append([linha[0],linha[1],linha[2],linha[3],media])
 
-c = csv.writer(open("leituraPython.csv", "w"), delimiter=";",quotechar='"',quoting=csv.QUOTE_MINIMAL)
+c = csv.writer(open("leituraPython.csv", "w"), delimiter=";",quotechar="'",quoting=csv.QUOTE_MINIMAL)
 for media in medias:  
     c.writerow(media)
